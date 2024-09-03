95ff502184a4230f38e5487a4da1fce5422482d6
Simon Danielsson <sd223ah.student.lnu.se>
2024-09-03 11:50:33 +0200

    Add an integer data type class

--- src/js/components/terminal/lib/editor/lib/dataTypes.js
+++ src/js/components/terminal/lib/editor/lib/dataTypes.js
@@ -48,9 +48,72 @@ class Pointer {
    * Returns a identical copy of the variable to be passed by value.
    *
    * @returns {*} - The identical variable copy.
+   * @param {string} id - The unique id identifier for the new value reference.
    * @throws {Error} - If method has no valid implementation.
    */
-  passByValue () {
+  passByValue (id) {
     throw new Error('Method getValue not implemented.')
+  }
+}
+
+// --- INTEGER --- //
+
+/**
+ * Represents an integer data type.
+ */
+export class Integer extends Pointer {
+  /**
+   * The integer value.
+   *
+   * @type {number}
+   */
+  #value
+
+  /**
+   * Constrcuts an integer variable.
+   *
+   * @param {number} value - The integer value.
+   * @param {string} id - The unique id identifier.
+   * @throws {Error} - If assigned value is not an integer.
+   */
+  constructor (value, id) {
+    super(id)
+
+    this.setValue(value)
+  }
+
+  /**
+   * Sets a new integer value.
+   *
+   * @override
+   * @param {number} newValue - The new integer value.
+   * @throws {Error} - If assigned value is not an integer.
+   */
+  setValue (newValue) {
+    if (!Number.isInteger(newValue)) {
+      throw new Error('Invalid value assigned to integer.')
+    }
+    this.#value = newValue
+  }
+
+  /**
+   * Returns the value of the integer variable.
+   *
+   * @override
+   * @returns {number} - The integer value.
+   */
+  getValue () {
+    return this.#value
+  }
+
+  /**
+   * Returns a new integer copy of this instance.
+   *
+   * @override
+   * @param {string} id - The new unique id to be assigned to the copy.
+   * @returns {Integer} - The integer copy.
+   */
+  passByValue (id) {
+    return new Integer(this.#value, id)
   }
 }
